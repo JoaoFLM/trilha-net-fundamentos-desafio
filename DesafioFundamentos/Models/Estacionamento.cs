4d29d155cf008259ee843a340ecd191242a469ba
Joao Felipe <lopesmonteirojoaofelipe@gmail.com>
2024-05-01 19:13:29 -0300

    Finalizado

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -32,12 +32,12 @@ namespace DesafioFundamentos.Models
                 // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
                 // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
                 // *IMPLEMENTE AQUI*
-                int horas = Console.ReadLine();
+                int horas = Convert.ToInt32(Console.ReadLine());
                 decimal valorTotal = precoInicial+(precoPorHora*horas); 
 
                 // TODO: Remover a placa digitada da lista de veículos
                 // *IMPLEMENTE AQUI*
-                veiculos.remove(placa);
+                veiculos.Remove(placa);
 
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
             }
@@ -55,7 +55,7 @@ namespace DesafioFundamentos.Models
                 Console.WriteLine("Os veículos estacionados são:");
                 // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
                 // *IMPLEMENTE AQUI*
-                foreach(string veiculo : veiculos){
+                foreach(string veiculo in veiculos){
                     Console.WriteLine(veiculo);
                 }
             }
